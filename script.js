ff2685c1e56a19b4e3c0a6e05f352ecb2d6bd025
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2023-02-18 12:23:41 -0800

    almoust finish burger menu styles

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const toggle = document.getElementById('toggle')
 const showNav = document.getElementById('show-nav')
-const closer = document.getElementById('closer')
 
 toggle.addEventListener('click', () => {
     showNav.classList.toggle('show')
     showNav.classList.toggle('hide')
-})
-closer.addEventListener('click', () => {
-    mobile.classList.toggle('show')
-    mobile.classList.toggle('hide')
 })
